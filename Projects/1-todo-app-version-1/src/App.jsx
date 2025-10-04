cf6eb85aceb234d1c14ec5e5e69e854bc7837563
max steel <maindatahub@gmail.com>
2025-10-04 05:30:44 +0000

    added the task 1 to app file

--- Projects/1-todo-app-version-1/src/App.jsx
+++ Projects/1-todo-app-version-1/src/App.jsx
@@ -1,7 +1,7 @@
 
 import AppName from "./components/AppName";
 import TodoAdd from "./components/TodoAdd";
-
+import Task1 from "./components/Task1";
 
 
 
@@ -11,23 +11,9 @@ function App() {
 
       <AppName />
       <TodoAdd />
+      <Task1 />
 
-
-
-
-        <div class="row">
-          <div class="col-6">
-            Buy Milk>
-          </div>
-          <div class="col-4">
-            5/10/2025
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">Delete</button>
-          </div>
-        </div>
-
-
+      <div class="container text-center">
         <div class="row">
           <div class="col-6">
             Go To College
@@ -39,8 +25,6 @@ function App() {
             <button type="button" class="btn btn-danger">Delete</button>
           </div>
         </div>
-
-
       </div>
     </center>
   );
