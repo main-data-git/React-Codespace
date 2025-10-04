e9e33945a24eb41b46f7fda7dd7bfdfe0dd0358d
max steel <maindatahub@gmail.com>
2025-10-04 05:15:35 +0000

    Made the basic todo app without components

--- Projects/1-todo-app-version-1/src/App.jsx
+++ Projects/1-todo-app-version-1/src/App.jsx
@@ -1,21 +1,52 @@
-
-
 function App() {
-  
+  return (
+    <center class="todocontainer">
+      <h1>Todo App</h1>
 
-  return  <center class="todocontainer">
-    <h1>Todo App</h1>
-    
-    
-  </center>
-    
-  
+      
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter Todo Here" />
+          </div>
+          <div class="col-4">
+            <input type="date" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">Add</button>
+          </div>
+        </div>
+
+
+        <div class="row">
+          <div class="col-6">
+            Buy Milk>
+          </div>
+          <div class="col-4">
+            5/10/2025
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">Delete</button>
+          </div>
+        </div>
+
+
+        <div class="row">
+          <div class="col-6">
+            Go To College
+          </div>
+          <div class="col-4">
+            06/10/2025
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">Delete</button>
+          </div>
+        </div>
+
+
+      </div>
+    </center>
+  );
 }
 
-export default App
-
-
-
-
-
-
+export default App;
