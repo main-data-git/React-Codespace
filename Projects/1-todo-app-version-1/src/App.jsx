08828141b0601375c103dc35d4621a073c6c670e
max steel <maindatahub@gmail.com>
2025-10-04 04:53:24 +0000

    added heading

--- Projects/1-todo-app-version-1/src/App.jsx
+++ Projects/1-todo-app-version-1/src/App.jsx
@@ -4,11 +4,9 @@ function App() {
   
 
   return  <center class="todocontainer">
-    hello how are you 
-    what are yoiu doing
-    <h1>My Todo App</h1>
-    <input type="text" placeholder="Add a new task..." />
-    <button>Add Task</button>
+    <h1>Todo App</h1>
+    
+    
   </center>
     
   
