42dc41841065d5d52a65bee8166e4e1f30d3ff6b
max steel <maindatahub@gmail.com>
2025-10-04 06:29:19 +0000

    ADDED THE TITLE AND THE INTRO TEXT

---
+++ Projects/Bharat Clock/src/Components/Heading.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Heading() {
+  return (
+    <center className="heading">
+      <h1>Bharat Clock</h1>
+    </center>
+  );
+}
+
+export default Heading;
---
+++ Projects/Bharat Clock/src/Components/IntroLine.jsx
@@ -0,0 +1,10 @@
+
+
+function IntroLine() {
+  return (
+    <center className="intro-line">
+      <h3>This is the clock that shows the time in Bharat at all the times </h3>
+    </center>
+  );
+}
+export default IntroLine;
